b3a004bcfcede046d3c88c2af59bd5e1225bdfc4
Knut Kirkhorn <knutkirk@hotmail.com>
2024-02-10 01:30:00 +0100

    Tweak link hover text colors

--- components/header.tsx
+++ components/header.tsx
@@ -6,13 +6,13 @@ export default function Header() {
 	return (
 		<nav className="hidden md:flex gap-8 right-0 justify-center">
 			<Link
-				className="text-lg font-medium text-slate-800 hover:text-black transition duration-100 ease-in-out"
+				className="text-lg font-medium text-slate-800 hover:text-slate-950 transition duration-100 ease-in-out"
 				href="/"
 			>
 				Home
 			</Link>
 			<Link
-				className="text-lg font-medium text-slate-800 hover:text-black transition duration-100 ease-in-out"
+				className="text-lg font-medium text-slate-800 hover:text-slate-950 transition duration-100 ease-in-out"
 				// href="/projects" - TODO: make it into own page
 				href="https://github.com/knutkirkhorn"
 				rel="noopener noreferrer"
@@ -24,7 +24,7 @@ export default function Header() {
 				</span>
 			</Link>
 			<Link
-				className="text-lg font-medium text-slate-800 hover:text-black transition duration-100 ease-in-out"
+				className="text-lg font-medium text-slate-800 hover:text-slate-950 transition duration-100 ease-in-out"
 				href="/contact"
 			>
 				Contact
